d63334879170c896e536a39a8b78849f88ed6b18
leozitor <leozitor@gmail.com>
2022-04-11 20:12:57 -0700

    heroku commit

---
+++ scripts.sh
@@ -0,0 +1,11 @@
+# reset db
+rm -rf db.sqlite3
+rm -rf webapp/migrations
+
+## populate db
+python manage.py makemigrations webapp
+python manage.py migrate
+python manage.py loaddata db-import.json
+
+# creating super user
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@gmail.com', 'admin')" | python manage.py shell
